7157995daad269ad6617efce5893ddf1965104a0
Erik Telepovský <erik.telepovsky@gmail.com>
2020-06-08 16:18:46 +0200

    v1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-writing',
-    version='1.0.1',
+    version='1.0.2',
     description='Basic blog app for Django',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
