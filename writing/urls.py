d51e95a0f956ab1578f6c7f07a5df6064fee63ee
Erik Telepovský <erik.telepovsky@gmail.com>
2020-06-18 20:03:03 +0200

    removed articles from URL

--- writing/urls.py
+++ writing/urls.py
@@ -6,6 +6,7 @@ app_name = 'writing'
 
 urlpatterns = [
     # path('category/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    path(pgettext_lazy('url', 'articles/'), ArticleListView.as_view(), name='article_list'),
+    # path(pgettext_lazy('url', 'articles/'), ArticleListView.as_view(), name='article_list'),
+    path('', ArticleListView.as_view(), name='article_list'),
     path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
